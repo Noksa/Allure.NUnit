6c4411dafc9908e580c5a5e6379f51796ef42159
Noksa <a.danilin@iml.ru>
2019-08-23 14:04:56 +0300

    Done #32

--- Allure/Commons/Helpers/ReflectionHelper.cs
+++ Allure/Commons/Helpers/ReflectionHelper.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 
 namespace Allure.Commons.Helpers
 {
@@ -41,21 +42,42 @@ namespace Allure.Commons.Helpers
             switch (memberType)
             {
                 case MemberTypes.Field:
-                    var fieldInfo = ((FieldInfo) member);
+                    var fieldInfo = (FieldInfo) member;
                     value = fieldInfo.GetValue(fieldInfo.IsStatic ? null : obj);
                     break;
                 case MemberTypes.Property:
-                    var propInfo = ((PropertyInfo) member);
+                    var propInfo = (PropertyInfo) member;
                     value = propInfo.GetValue(propInfo.GetAccessors().Any(a => a.IsStatic) ? null : obj);
                     break;
                 case MemberTypes.Method:
-                    var methodInfo = ((MethodInfo) member);
-                    var @params = methodInfo.GetParameters().ToArray();
-                    value = methodInfo.Invoke(methodInfo.IsStatic ? null : obj, @params);
+                    var methodInfo = (MethodInfo) member;
+                    value = methodInfo.IsDefined(typeof(ExtensionAttribute), false) ? methodInfo.Invoke(null, new[] {obj}) : methodInfo.Invoke(methodInfo.IsStatic ? null : obj, null);
+
                     break;
             }
 
             return value;
+        }
+
+        internal static MethodInfo GetExtensionMethod(this Type paramType, string methodName)
+        {
+            foreach (var item in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                var types = item.GetTypes().Where(t => t.IsSealed && !t.IsGenericType && !t.IsNested);
+
+                foreach (var type in types)
+                {
+                    var method = type.GetMethod(methodName,
+                        BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Public);
+                    if (method == null) continue;
+                    if (method.GetParameters().Length == 0) continue;
+                    if (!method.IsDefined(typeof(ExtensionAttribute), false)) continue;
+                    if (method.GetParameters()[0].ParameterType != paramType) continue;
+                    return method;
+                }
+            }
+
+            return null;
         }
 
         internal static MemberInfo GetMember(Type type, string memberName)
@@ -71,6 +93,9 @@ namespace Allure.Commons.Helpers
                     .FirstOrDefault(fi =>
                         fi.IsLiteral && !fi.IsInitOnly &&
                         fi.Name == memberName);
+
+            if (member == null) member = GetExtensionMethod(type, memberName);
+
             return member;
         }
 
@@ -96,24 +121,27 @@ namespace Allure.Commons.Helpers
                     switch (memberType)
                     {
                         case MemberTypes.Field:
-                            memberValue = ((FieldInfo)member).GetValue(null);
+                            memberValue = ((FieldInfo) member).GetValue(null);
                             break;
                         case MemberTypes.Property:
-                            memberValue = ((PropertyInfo)member).GetValue(null);
+                            memberValue = ((PropertyInfo) member).GetValue(null);
                             break;
                     }
                 }
             }
+
             return memberValue;
         }
 
         internal static FieldInfo GetBackingField(this Type type, string name, bool partialMatch = false)
         {
             var msg = partialMatch ? ", which contains" : "";
-            var field = type.GetFields(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public).FirstOrDefault(p =>
-                 p.Name.EndsWith("__BackingField") && partialMatch ? p.Name.Contains(name) : p.Name.Equals(name));
+            var field = type.GetFields(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public)
+                .FirstOrDefault(p =>
+                    p.Name.EndsWith("__BackingField") && partialMatch ? p.Name.Contains(name) : p.Name.Equals(name));
             if (field != null) return field;
-            throw new MissingFieldException($"Cant find backing field in type {nameof(type)} with name{msg} \"{name}\"");
+            throw new MissingFieldException(
+                $"Cant find backing field in type {nameof(type)} with name{msg} \"{name}\"");
         }
     }
 }
